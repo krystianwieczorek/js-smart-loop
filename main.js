5d0c28ba9f4b9cdb3c234ea2cf6710a571aca162
krystianwieczorek <krystian.adam.wieczorek@gmail.com>
2018-02-24 20:08:22 +0100

    add comments

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+// Function definition
 function multiplyElements(multiplier, arrayLength) {
     for (var i = 0; i < arrayLength; i++) {
         console.log(multiplier * i)
     }
 }
 
+// Call function
 multiplyElements(4, 5);
